1483c772c5074ba188d62438d0f1f348be18d342
Oduyale Enoch <enoch.oduyale@reventtechnologies.com>
2026-04-08 20:55:19 +0100

    implement debit wallet functionality and add new BillService structure

--- internal/wallet/service.go
+++ internal/wallet/service.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"errors"
 	"fintech/internal/auth"
 	"fintech/internal/models"
 	"strings"
@@ -35,7 +36,6 @@ func (s *WalletService) GetBalance(c *fiber.Ctx) error {
 	})
 }
 func generateTransactionReference() string {
-	// Example: TXN_2D2B5B2D18CC40D5A8A5E6A22B92C8B9
 	return "TXN_" + strings.ToUpper(strings.ReplaceAll(uuid.NewString(), "-", ""))
 }
 
@@ -74,5 +74,50 @@ func (s *WalletService) CreditWallet(c *fiber.Ctx, amount int64, desc string) er
 
 		return tx.Create(&transaction).Error
 
+	})
+}
+
+func (s *WalletService) DebitWallet(c *fiber.Ctx, amount int64, reference string, category string, desc string) error {
+
+	userID, err := auth.GetUserIDFromContext(c)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return s.DB.Transaction(func(tx *gorm.DB) error {
+		var wallet models.Wallet
+
+		// 1. Get wallet and lock the row so no other process can change it
+		if err := tx.Set("gorm:query_option", "FOR UPDATE").Where("user_id = ?", userID).First(&wallet).Error; err != nil {
+			return err
+		}
+
+		// 1. check balance
+
+		if wallet.Balance < amount {
+			return errors.New("insufficient funds")
+		}
+
+		// 3. deduct balance
+
+		wallet.Balance -= amount
+		if err := tx.Save(&wallet).Error; err != nil {
+			return err
+		}
+
+		reference := generateTransactionReference()
+		// 4. create transaction record
+
+		transaction := models.Transaction{
+			WalletID:    wallet.ID,
+			Amount:      amount,
+			Type:        models.Debit,
+			Status:      models.Pending, // we start as pending until API confirms
+			Reference:   reference,
+			Description: desc,
+			Category:    category,
+		}
+
+		return tx.Create(&transaction).Error
 	})
 }
